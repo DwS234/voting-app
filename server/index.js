ce28af3cb0be0571c6586b1e35af59cffe87d909
Dawid <dawid.gonisiewski2@gmail.com>
2018-05-04 10:49:06 +0200

    'fsd'

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(session({secret: process.env.SECRET, resave: false, saveUninitialized: f
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.resolve("client/build")));
 
 
 app.get("/api/getVotings", (req, res) => {
